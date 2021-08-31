318df6f248006c666634108fd77597a9a00f1a00
Alex Pylko <alexpylko@gmail.com>
2021-08-31 19:45:29 +0200

    Add worker interchangers (#5)

--- lib/scompler/kafka/sync_topics.rb
+++ lib/scompler/kafka/sync_topics.rb
@@ -4,7 +4,7 @@ module Scompler
   module Kafka
     class SyncTopics
       def initialize(classes)
-        @classes = classes
+        @classes = Array(classes)
         @app_topics = []
       end
 
@@ -40,25 +40,62 @@ module Scompler
         return if removed_topics.blank?
 
         p "Found #{removed_topics.size} removed topics"
-        removed_topics.each do |topic_name|
-          p "Remove #{topic_name} topic"
-          kafka.delete_topic(topic_name)
+        removed_topics.each do |name|
+          p "Remove #{name} topic"
+          kafka.delete_topic(name)
         end
       end
 
       def sync
-        p "Going to sync #{classes.size} topics"
+        p "Going to sync #{classes.size} producers"
         classes.each do |klass|
-          klass.topics.each do |topic_name, _|
-            # TODO: Add topic options updating
-            topic_name = topic_mapper.outgoing(topic_name)
-            @app_topics << topic_name
-            next if kafka_topics.include?(topic_name)
-
-            p "Create #{topic_name} topic"
-            kafka.create_topic(topic_name)
-          end
+          sync_topics_for(klass)
         end
+      end
+
+      def sync_topics_for(klass)
+        p "Going to sync #{klass} topics"
+        klass.topics.each do |name, topic|
+          options = topic.options
+
+          full_name = topic_mapper.outgoing(name)
+          @app_topics << full_name
+
+          create_or_alter_topic(full_name, options)
+        end
+      end
+
+      def create_or_alter_topic(name, options = {})
+        if kafka_topics.include?(name)
+          return if options.blank?
+
+          alter_partitions_for(name, options.slice(:num_partitions, :timeout))
+          alter_configs_for(name, options.fetch(:config))
+        else
+          create_topic(name, options)
+        end
+      end
+
+      def create_topic(name, options)
+        p "Create #{name} topic with params #{options}"
+        kafka.create_topic(name, options)
+      end
+
+      def alter_configs_for(name, configs = {})
+        return if configs.blank?
+
+        existing_configs = kafka.describe_topic(name, configs.keys)
+        return if existing_configs == configs
+
+        p "Alter #{name} topic configs with params #{configs}"
+        kafka.alter_topic(name, configs)
+      end
+
+      def alter_partitions_for(name, num_partitions:, timeout: 30)
+        return if num_partitions.nil? || (num_partitions == kafka.partitions_for(name))
+
+        p "Alter #{name} topic with #{num_partitions} partitions"
+        kafka.create_partitions_for(name, num_partitions: num_partitions, timeout: timeout)
       end
     end
   end
